9ca76419ee73d94afcbca2da78c9768e40135565
Borisenko Natalli <nata251284@gmail.com>
2023-01-28 20:55:56 +0300

    Произведены корректировки

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -5,17 +5,10 @@ a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */ 
 
-/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
-какое число большее, а какое меньшее.
-a = 5; b = 7 -> max = 7
-a = 2 b = 10 -> max = 10
-a = -9 b = -3 -> max = -3
-*/ 
-
-Console.WriteLine("Введите первое число: ");
+Console.Write("Введите первое число: ");
 int numberOne = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите второе число: ");
+Console.Write("Введите второе число: ");
 int numberTwo = Convert.ToInt32(Console.ReadLine());
 
 int max = numberOne;
