1833698e4263d965d21ce320aab7ca2292369255
Borisenko Natalli <nata251284@gmail.com>
2023-01-28 22:28:29 +0300

    Программа определяет четные числа от 1 до N

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.Write("Введите число N: ");
+string numberStr = Console.ReadLine();
+int N = Convert.ToInt32(numberStr);
+
+Console.Write("Четные числа от 1 до "+N + " -> ");
+
+for (int i = 1; i <= N; i++)
+{
+    if(i%2 == 0)
+    {
+        Console.Write(i+", ");
+    }
+}
+if(N<0)
+{
+    Console.WriteLine("Введите число больше нуля.");
+}
