5ea9f7eac8b42ede61a4de484c4692920869271f
Borisenko Natalli <nata251284@gmail.com>
2023-01-28 21:48:33 +0300

    Программа определяет четное число или нет

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.Write("Введите число: ");
+string numberstr = Console.ReadLine();
+int number = Convert.ToInt32(numberstr);
+
+if (number%2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+  Console.WriteLine("Нет");  
+}
